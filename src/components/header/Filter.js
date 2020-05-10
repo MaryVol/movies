913b7e7de80f688db633836086693036663c7800
MaryVol <meryswhite@icloud.com>
2020-05-10 19:51:08 +0300

    correction of filter buttons

--- src/components/header/Filter.js
+++ src/components/header/Filter.js
@@ -2,19 +2,40 @@ import React from 'react';
 import styles from './Header.module.css';
 
 class Filter extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            check1: true,
+            check2: false,
+        }
+    }
+    changeVal(){
+        this.setState({
+            check1: false,
+            check2: true,
+        })
+    }
+    changeValBack(){
+        this.setState({
+            check1: true,
+            check2: false,
+        })
+    }
+
+
     render() { 
         return ( 
                 <div className={styles.filterWrapper}>
                     <span>{this.props.name}</span>
                     <div className={styles.filterBtns}>
                         <label>
-                            <input type="radio" name="radio" checked/>
+                            <input type="radio" value="check1" checked={this.state.check1} onClick={()=> this.changeValBack()}/>
                             <div className={styles.box}>
                                 <span>{this.props.firstval}</span>
                             </div>
                         </label>
                         <label>
-                            <input type="radio" name="radio"/>
+                            <input type="radio" value="check2" checked={this.state.check2} onClick={()=> this.changeVal()} />
                             <div className={styles.box}>
                                 <span>{this.props.secondval}</span>
                             </div>

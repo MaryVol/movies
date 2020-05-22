0900f862cfabf887affee123efaf038b2984588f
MaryVol <meryswhite@icloud.com>
2020-05-22 15:34:27 +0300

    commit

--- src/components/mainContent/MainContent.js
+++ src/components/mainContent/MainContent.js
@@ -11,30 +11,6 @@ class MainContent extends React.Component {
     super(props)
     this.state = {
         currentMovie: undefined,
-    //     movies: [
-    //     {
-    //         id: 0,
-    //         title: "Kill Bill: Vol2",
-    //         release_date: "2004",
-    //         poster_path: "https://i.pinimg.com/originals/64/82/ef/6482ef51ce0b437986ec477e0d46f30c.jpg",
-    //         overview: "film description",
-    //         runtime: "154",
-    //         rating: "4.2",
-    //         genre: "Action & Adventure",
-    //         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero laudantium minima quas neque quos eius placeat dignissimos voluptatum sequi modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, consequatur? Dolorum et nam quod aperiam, numquam consequatur excepturi debitis reiciendis."
-    //     },
-    //     {
-    //         id: 1,
-    //         title: "Kill Bill: Vol1",
-    //         release_date: "2003",
-    //         poster_path: "https://www.film.ru/sites/default/files/movies/posters/tumblr_n4k5nvk6c01tzuuzio1_1280.jpg",
-    //         overview: "film description",
-    //         runtime: "160",
-    //         rating: "4.0",
-    //         genre: "Action & Adventure",
-    //         description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero laudantium minima quas neque quos eius placeat dignissimos voluptatum sequi modi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, consequatur? Dolorum et nam quod aperiam, numquam consequatur excepturi debitis reiciendis."
-    //     }
-    // ]
   }
 }
 

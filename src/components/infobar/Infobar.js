1bad817fe44d4bd60f1cb2b0b45555ac66dedbcf
MaryVol <meryswhite@icloud.com>
2020-05-24 17:33:01 +0300

    commit

--- src/components/infobar/Infobar.js
+++ src/components/infobar/Infobar.js
@@ -2,21 +2,28 @@ import React from 'react';
 import styles from './infobar.module.css';
 import Counter from './Counter';
 import Toggle from '../header/Toggle';
+import MovieData from '../mainContent/movies/movies.json';
+
 
 let sortOptions = [ 'Title', 'Genre' ];
 
 class Infobar extends React.Component {
   constructor(props){
     super(props)
+    this.toggleSortTitle = this.toggleSortTitle.bind(this)
     this.state = {
         names: 'Sort by',
         sortBy: 'Title',
         sortOptions: [ 'Title', 'Genre' ],
-        // active: this.props.firstval,
-        // checked: sortOptions[0]
+        MovieData: {},
       }
-    // this.handleChange = this.handleChange.bind(this);
 }
+sortByTitle () {
+  const {MovieData} = this.state
+  let newMovieData = MovieData
+  newPostList = postList.sort((a, b) => a.title > b.title)
+}
+
   render(){
     return (
         <div className={styles.infoBar}>

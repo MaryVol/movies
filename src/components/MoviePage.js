23f840acb28fc00a95febf1a5ca92fb5763fa7dd
MaryVol <meryswhite@icloud.com>
2020-06-08 01:21:25 +0200

    testing

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,31 +1,45 @@
-import React from 'react';
-import styles from './mainContent/MainContent.module.css';
+import React from "react";
+import styles from "./mainContent/MainContent.module.css";
 
 class MoviePage extends React.Component {
-
-
-render(){
-    let movie = this.props.movie
+  render() {
+    let movie = this.props.movie;
     return (
       <main>
         <div className={styles.container}>
-        <div className={styles.movieBageWrapper} >
-                <div className={styles.container}>
-                    <h3><b>netflix</b>roulette</h3> 
-                    <p className={styles.arrow} onClick={() => this.props.onReturnBack(movie)}><i className="fas fa-chevron-left"></i>Back</p>
-                    <div className={styles.movieCard}>
-                        <img src={movie.poster_path} alt="movie poster"/>
-                        <div className={styles.textWrap}>
-                            <h2>{movie.title} <span className={styles.rating}>{movie.rating}</span></h2>
-                            <p><span className={styles.number}>{movie.release_date}</span>year<span className={styles.number}>{movie.runtime}</span>min</p>
-                            <p>{movie.description}</p>
-                        </div>
-                    </div> 
+          <div className={styles.movieBageWrapper}>
+            <div className={styles.container}>
+              <h3>
+                <b>netflix</b>roulette
+              </h3>
+              <p
+                className={styles.arrow}
+                onClick={() => this.props.onReturnBack(movie)}
+              >
+                <i className="fas fa-chevron-left"></i>Back
+              </p>
+              <div className={styles.movieCard}>
+                <img src={movie.poster_path} alt="movie poster" />
+                <div className={styles.textWrap}>
+                  <h2>
+                    {movie.title}{" "}
+                    <span className={styles.rating}>{movie.rating}</span>
+                  </h2>
+                  <p>
+                    <span className={styles.number}>{movie.release_date}</span>
+                    year<span className={styles.number}>{movie.runtime}</span>
+                    min
+                  </p>
+                  <p>{movie.description}</p>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
         <div className={styles.footer}>
-            <h3><b>netflix</b>roulette</h3>
+          <h3>
+            <b>netflix</b>roulette
+          </h3>
         </div>
       </main>
     );
--- src/components/mainContent/MovieList.js
+++ src/components/mainContent/MovieList.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import styles from './MainContent.module.css';
-
+import React from "react";
+import styles from "./MainContent.module.css";
 
 function MovieList(props) {
-    return (
-        <div className={styles.movieWrapper}>
-          {props.movies.map((mData, id)=>{
-              return <div className={styles.cardWrapper} key={mData.id} onClick={() => props.onChange(mData)}>
-                <img  src={mData.poster_path}></img>
-                <h4>{mData.title}</h4>
-                <small>{mData.genre}</small>
-                <h5>{mData.release_date}</h5>
-              </div>
-
-          })}
-        </div>
-    );
-  }
+  return (
+    <div className={styles.movieWrapper}>
+      {props.movies.map((mData, id) => {
+        return (
+          <div
+            className={styles.cardWrapper}
+            key={mData.id}
+            onClick={() => props.onChange(mData)}
+          >
+            <img src={mData.poster_path}></img>
+            <h4>{mData.title}</h4>
+            <small>{mData.genre}</small>
+            <h5>{mData.release_date}</h5>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
 
 export default MovieList;

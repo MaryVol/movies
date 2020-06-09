a7c2121a6259f90edac5d9f3462df960a9623efc
MaryVol <meryswhite@icloud.com>
2020-06-09 14:10:15 +0200

    commit

---
+++ src/components/header/Toggle.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './Header.module.css'; 
+
+
+class Toggle extends React.Component {
+    
+    render() { 
+        return ( 
+                <div className={styles.filterWrapper}>
+                    <span>{this.props.name}</span>
+                    <div className={styles.filterBtns}>
+                        {this.props.options.map((option) => {
+                        return <label key={option.value}>
+                            <input type="radio" checked={this.props.value === option.value} onChange={() => this.props.onChange(option.value)} />
+                            <div className={styles.box}>
+                                <span>{option.displayName}</span>
+                            </div>
+                        </label>
+                        })}
+                    </div>
+                </div>
+         );
+    }
+}
+ 
+export default Toggle;

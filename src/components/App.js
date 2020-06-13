37f35fc5e39e01aaf2eb58f10108067a7d2e5c0c
MaryVol <meryswhite@icloud.com>
2020-06-13 18:08:26 +0200

    testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import MainContent from './MainContent';
-
+import React, { Component } from "react";
+import Header from "./Header";
+import MainContent from "./MainContent";
 
 class App extends Component {
-
-
-    render(){
-        return(
-        <div>
-            <Header />
-            <MainContent />
-            
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <MainContent />
+      </div>
+    );
+  }
 }
 
 export default App;

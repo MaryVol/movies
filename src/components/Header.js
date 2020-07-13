17cf4d2932d3b0aade408ea0f154dddcfedecdab
MaryVol <meryswhite@icloud.com>
2020-07-13 17:10:29 +0200

    redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,17 +10,6 @@ let searchByOptions = [
 ];
 
 class Header extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     names: "Search by",
-  //     searchBy: "release_date",
-  //     searchByOptions: [
-  //       { value: "release_date", displayName: "Release date" },
-  //       { value: "rating", displayName: "Rating" },
-  //     ],
-  //   };
-  // }
 
   render() {
     return (
@@ -32,9 +21,9 @@ class Header extends React.Component {
           <h1>find your movie</h1>
           <Searcher />
           <Toggle
-            name={this.state.names}
-            options={this.state.searchByOptions}
-            value={this.state.searchBy}
+            name={names}
+            options={searchByOptions}
+            value={searchBy}
             onChange={(searchBy) => this.setState({ searchBy })}
           />
         </div>

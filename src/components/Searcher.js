1fda8beee187fbec6ecaf8985268b5dab8cbcce7
MaryVol <meryswhite@icloud.com>
2020-07-15 14:56:38 +0200

    redux

--- src/components/Searcher.js
+++ src/components/Searcher.js
@@ -4,6 +4,10 @@ import $ from "jquery";
 import { connect } from "react-redux";
 
 class Searcher extends React.Component {
+  constructor(props) {
+    super(props);
+    this.input = React.createRef();
+  }
   performSearch(searchTerm) {
     const urlString =
       "https://api.themoviedb.org/3/search/movie?api_key=e530f5fe1f77ddf63766eee3c707e2fb&language=en-US&page=1&include_adult=false&query=" +
@@ -24,23 +28,21 @@ class Searcher extends React.Component {
   render() {
     return (
       <div className={styles.searcherWrapper}>
-        <form onSubmit={(event) => event.target.elements.query}>
+        <form
+          onSubmit={(searchQuery) =>
+            this.props.dispatch({
+              type: "SEARCH",
+              searchQuery: this.input.current.value,
+            })
+          }
+        >
           <input
             type="text"
             className={styles.search}
             placeholder="Enter a movie"
-            name="query"
+            ref={this.input}
           />
-          <button
-            className={styles.srcBtn}
-            type="submit"
-            onClick={(searchQuery) =>
-              this.props.dispatch({
-                type: "SEARCH",
-                searchQuery: searchQuery,
-              })
-            }
-          >
+          <button className={styles.srcBtn} type="submit">
             Search
           </button>
         </form>

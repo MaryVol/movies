cb4b8aec35703a318e74314a2d705ab0d99ab7ed
MaryVol <meryswhite@icloud.com>
2020-07-22 16:41:20 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import App from "./components/App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducers/index";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
   reducer,
+  composeWithDevTools(applyMiddleware(...middleware)),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 

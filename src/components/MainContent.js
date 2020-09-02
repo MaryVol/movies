84183f5400e2e270d2a28485a50707f476fc5b5c
MaryVol <meryswhite@icloud.com>
2020-09-03 01:53:26 +0300

    films router

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,31 +2,37 @@ import React from "react";
 import MoviesPage from "./MoviesPage";
 import MoviePage from "./MoviePage";
 import { connect } from "react-redux";
-import { Route } from "react-router";
+import { Route, Switch } from "react-router";
 
 class MainContent extends React.Component {
   render() {
     const { movieList } = this.props;
-    if (this.props.currentMovie) {
-      return (
-        <Route
-          path="/:id"
-          render={(props) => (
-            <MoviePage
-              {...props}
-              movie={this.props.currentMovie}
-              onReturnBack={(currentMovie) =>
-                this.props.dispatch({
-                  type: "SHOW_MOVIE",
-                  currentMovie: null,
-                })
-              }
-            />
-          )}
-        />
-      );
-    }
-    return <MoviesPage movies={movieList} sortBy={this.props.sortBy} />;
+    // if (this.props.currentMovie) {
+    //   return (
+    //     <Route
+    //       path="/:id"
+    //       render={(props) => (
+    //         <MoviePage
+    //           {...props}
+    //           movie={this.props.currentMovie}
+    //           onReturnBack={(currentMovie) =>
+    //             this.props.dispatch({
+    //               type: "SHOW_MOVIE",
+    //               currentMovie: null,
+    //             })
+    //           }
+    //         />
+    //       )}
+    //     />
+    //   );
+    // }
+    // return <MoviesPage movies={movieList} sortBy={this.props.sortBy} />;
+    return (
+      <Switch>
+        <Route exact path="/" component={MoviesPage} />
+        <Route exact path="/movie/:movieId" component={MoviePage} />
+      </Switch>
+    );
   }
 }
 
